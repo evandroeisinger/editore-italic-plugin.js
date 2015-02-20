a4bde7f1e30b234a00b982cba3d85ca71677155c
Evandro Eisinger <evandroeisinger@gmail.com>
2015-02-20 16:40:31 -0200

    v1.0.0

---
+++ src/editore-italic-plugin.js
@@ -0,0 +1,34 @@
+(function(global, plugin) {
+  'use strict';
+
+  if (typeof define === 'function' && define.amd)
+    define('editore-italic-plugin', plugin);
+  else if (typeof exports !== 'undefined')
+    exports.EditoreItalicPlugin = plugin();
+  else
+    global.EditoreItalicPlugin = plugin();
+}(window, function() {
+  'use strict';
+
+  function EditoreItalicPlugin() {
+    var self = this;
+    // set plugin elements/props
+    self.button = document.createElement('button');
+    self.button.innerText = 'italic';
+    self.name = 'ItalicEditionPlugin';
+    self.tag = 'i';
+  }
+
+  EditoreItalicPlugin.prototype = {
+    action: function(field, e) {
+      e.preventDefault();
+      document.execCommand('italic', false, null);
+    },
+
+    destroy: function() {}
+  };
+
+
+  return EditoreItalicPlugin;
+}));
+    
